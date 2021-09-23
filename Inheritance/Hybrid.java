b77a4306a9dca4dec1882387a3d03c53e03a759a
nehaannvinu2607@gmail.com <nehaannvinu>
2021-09-23 10:00:44 +0530

    Added hybrid inheritance

--- Inheritance/Hybrid.java
+++ Inheritance/Hybrid.java
@@ -1,5 +1,39 @@
-package Inheritance;
-
-public class Hybrid {
-
+class C
+{
+   public void disp()
+   {
+	System.out.println("C");
+   }
 }
+
+class A extends C
+{
+   public void disp()
+   {
+	System.out.println("A");
+   }
+}
+
+class B extends C
+{
+   public void disp()
+   {
+	System.out.println("B");
+   }
+	
+}
+
+class Hybrid extends A
+{
+   public void disp()
+   {
+	System.out.println("Inside Hybrid");
+   }
+   public static void main(String args[]){
+
+	Hybrid obj = new Hybrid();
+	obj.disp();
+   }
+}
+
+//Output: Inside Hybrid
